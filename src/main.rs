f8f4afd62d263629099d71b72050aef852507a50
Gauravpadam <gauravpadam2@gmail.com>
2024-09-29 00:54:07 +0530

    Partial monologue implemented; socket emits remaining

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,35 @@
 use axum::routing::get;
+use serde_json::Value;
+use socketioxide::{extract::{Data, SocketRef}, SocketIo};
+use tower::ServiceBuilder;
+use tower_http::cors::CorsLayer;
 use tracing::info;
 use tracing_subscriber::FmtSubscriber;
 
+async fn on_connect(socket: SocketRef){
+    info!("Socket connected: {}", socket.id);
+
+    socket.on("message", |_socekt: SocketRef, Data::<Value>(data)| {
+        info!("You: {:?}", data);
+    })
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing::subscriber::set_global_default(FmtSubscriber::default())?;
 
-    let app = axum::Router::new().route("/", get(|| async { "Hello World!"}));
+    let (layer, io) = SocketIo::new_layer();
 
+    io.ns("/", on_connect);
+
+    let app = axum::Router::new()
+        .route("/", get(|| async { "Hello World!"}))
+        .layer(
+            ServiceBuilder::new()
+            .layer(CorsLayer::permissive())
+            .layer(layer),
+        );
+        
     info!("Starting server");
 
     axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
